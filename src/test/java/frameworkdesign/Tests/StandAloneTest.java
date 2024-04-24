5affac86b9bb311af0c01655ae798b52d7d4f8d8
Pujitha <pooji.gundapaneni@gmail.com>
2024-04-24 14:31:49 -0500

    updated standalone test

--- src/test/java/frameworkdesign/Tests/StandAloneTest.java
+++ src/test/java/frameworkdesign/Tests/StandAloneTest.java
@@ -20,6 +20,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+                //new commits are added
 		String itemsNeedsToBeAdded="ADIDAS ORIGINAL"; 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
